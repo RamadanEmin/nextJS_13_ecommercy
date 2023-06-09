387ec61a93b2944c341f070e354b71dfc47f03f6
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-09 12:39:02 +0300

    Added Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import axios from "axios";
+import ListProducts from "@/components/products/ListProducts";
 
-const HomePage =  () => {
 
+const getProducts = async () => {
+  const { data } = await axios.get(`${process.env.API_URL}/api/products`);
+  return data;
+};
+
+const HomePage = async ({ searchParams }) => {
+  const productData = await getProducts(searchParams);
+
+  return <ListProducts data={productData} />;
 };
 
 export default HomePage;
