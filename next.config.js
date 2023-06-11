20bf4bd36fbd391638f39f1d7f2630f3510aca71
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-11 16:35:34 +0300

    Added Header component

--- next.config.js
+++ next.config.js
@@ -7,7 +7,11 @@ const nextConfig = {
     API_URL: "http://localhost:3000",
     DB_URI: "mongodb://localhost:27017/buyitnow",
   },
-
+  images: {
+    domains: [
+      "res.cloudinary.com",
+    ]
+  },
 }
 
 module.exports = nextConfig
