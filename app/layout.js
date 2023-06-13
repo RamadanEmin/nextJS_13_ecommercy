2caca098f174769e24d291573bb5d6f8121d3f98
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-13 08:28:13 +0300

    Added 'Product details' component

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import './globals.css'
+import Header from '@/components/layouts/Header'
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head />
-      <body>
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head />
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    )
 }
