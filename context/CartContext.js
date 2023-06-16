7425bffef84fb9012ca13f2751b21a076d11aa38
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-16 13:48:40 +0300

    Added 'Cart context'

---
+++ context/CartContext.js
@@ -0,0 +1,22 @@
+'use client';
+
+import { createContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+
+    return (
+        <CartContext.Provider
+            value={{
+                cart
+            }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export default CartContext;
