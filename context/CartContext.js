dee1d89f064bc152140fd9f3c9b764c9f91c2df2
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-16 14:39:16 +0300

    Added Item to cart functionality

--- context/CartContext.js
+++ context/CartContext.js
@@ -1,17 +1,61 @@
 'use client';
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
+    useEffect(() => {
+        setCartToState();
+    }, []);
+
+    const setCartToState = () => {
+        setCart(localStorage.getItem('cart')
+            ? JSON.parse(localStorage.getItem('cart'))
+            : []
+        );
+    };
+
+    const addItemToCart = async ({
+        product,
+        name,
+        price,
+        image,
+        stock,
+        seller,
+        quantity = 1
+    }) => {
+        const item = {
+            product,
+            name,
+            price,
+            image,
+            stock,
+            seller,
+            quantity
+        };
+
+        const isItemExist = cart?.cartItems?.find((i) => i.product === item.product);
+
+        let newCartItems;
+
+        if (isItemExist) {
+            newCartItems = cart?.cartItems?.map((i) => i.product === isItemExist.product ? item : i)
+        } else {
+            newCartItems = [...(cart?.cartItems || []), item];
+        }
+
+        localStorage.setItem("cart", JSON.stringify({ cartItems: newCartItems }));
+        setCartToState();
+    };
 
     return (
         <CartContext.Provider
             value={{
-                cart
+                cart,
+                addItemToCart
             }}
         >
             {children}
