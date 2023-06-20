49603fdb03c71891b7ce5e8d128faf8b02b966a9
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-20 15:16:12 +0300

    Added 'User register' function

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,5 +1,7 @@
 'use client';
 
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { createContext, useState } from "react";
 
 const AuthContext = createContext();
@@ -7,6 +9,25 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
+
+    const router = useRouter();
+
+    const registerUser = async ({ name, email, password }) => {
+        try {
+            const { data } = await axios.post(`${process.env.API_URL}/api/auth/register`,
+                {
+                    name,
+                    email,
+                    password
+                });
+
+            if (data?.user) {
+                router.push('/');
+            }
+        } catch (error) {
+            setError(error?.response?.data?.message);
+        }
+    };
 
     const clearErrors = () => {
         setError(null);
@@ -17,6 +38,9 @@ export const AuthProvider = ({ children }) => {
             value={{
                 user,
                 error,
+                setUser,
+                registerUser,
+                clearErrors
             }}
         >
             {children}
