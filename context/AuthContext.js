48142a07185e2cc9f7d1d0505a73cb02637a8631
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-26 08:33:58 +0300

    Added new address function

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -29,6 +29,18 @@ export const AuthProvider = ({ children }) => {
         }
     };
 
+    const addNewAddress = async (address) => {
+        try {
+            const { data } = await axios.post(`${process.env.API_URL}/api/address`, address);
+
+            if (data) {
+                router.push('/me');
+            }
+        } catch (error) {
+            setError(error?.response?.data?.message);
+        }
+    };
+
     const clearErrors = () => {
         setError(null);
     };
@@ -40,6 +52,7 @@ export const AuthProvider = ({ children }) => {
                 error,
                 setUser,
                 registerUser,
+                addNewAddress,
                 clearErrors
             }}
         >
