f3cbb1e0e8e49732ac65d14fb2dd21b04612fbbd
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-28 12:15:45 +0300

    Added Get adresses function

--- backend/controllers/addressControllers.js
+++ backend/controllers/addressControllers.js
@@ -1,4 +1,5 @@
 import Address from "../models/address";
+import ErrorHandler from "../utils/errorHandler";
 
 export const newAddress = async (req, res) => {
     req.body.user = req.user._id;
@@ -12,4 +13,14 @@ export const getAddresses = async (req, res) => {
     const addresses = await Address.find({ user: req.user._id });
 
     res.status(200).json({ addresses });
+};
+
+export const getAddress = async (req, res) => {
+    const address = await Address.findById(req.query.id);
+
+    if (!address) {
+        return next(new ErrorHandler('Address not found', 404));
+    }
+
+    res.status(200).json({ address });
 };
