bd7dfe0a857e25a521b3f6732f3b03bec27c9dd1
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-29 21:18:04 +0300

    Added Update and Delete address functions

--- backend/controllers/addressControllers.js
+++ backend/controllers/addressControllers.js
@@ -24,3 +24,27 @@ export const getAddress = async (req, res) => {
 
     res.status(200).json({ address });
 };
+
+export const updateAddress = async (req, res) => {
+    let address = await Address.findById(req.query.id);
+
+    if (!address) {
+        return next(new ErrorHandler('Address not found', 404));
+    }
+
+    address = await Address.findByIdAndUpdate(req.query.id, req.body);
+
+    res.status(200).json({ address });
+};
+
+export const deleteAddress = async (req, res) => {
+    let address = await Address.findById(req.query.id);
+
+    if (!address) {
+        return next(new ErrorHandler('Address not found', 404));
+    }
+
+    await address.deleteOne();
+
+    res.status(200).json({ success: true });
+};
