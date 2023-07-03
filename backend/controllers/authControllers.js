61bea5b089f49e93f341e9231c3b547d3718ccd4
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-03 09:20:34 +0300

    Include updateProfile in authControllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -11,3 +11,24 @@ export const registerUser = async (req, res) => {
 
     res.status(201).json({ user });
 };
+
+export const updateProfile = async (req, res) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    if (req.files.length > 0) {
+        const uploader = async (path) => await uploads(path, "ecommerce/images");
+
+        const file = req.files[0];
+        const { path } = file;
+
+        const avatarResponse = await uploader(path);
+        fs.unlinkSync(path);
+        newUserData.image = avatarResponse;
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, newUserData);
+    res.status(200).json({ user });
+};
