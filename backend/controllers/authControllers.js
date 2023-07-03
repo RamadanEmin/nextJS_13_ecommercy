81f2a09a49d33fdb6b9dbb30611497ce2f878d73
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-03 11:06:46 +0300

    Include updatePassword in authControllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -32,3 +32,18 @@ export const updateProfile = async (req, res) => {
     const user = await User.findByIdAndUpdate(req.user._id, newUserData);
     res.status(200).json({ user });
 };
+
+export const updatePassword = async (req, res, next) => {
+    const user = await User.findById(req.user._id).select('+password');
+
+    const isPasswordMatched = await bcrypt.compare(req.body.currentPassword, user.password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Old password is incorrect', 400));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    res.status(200).json({ success: true });
+};
