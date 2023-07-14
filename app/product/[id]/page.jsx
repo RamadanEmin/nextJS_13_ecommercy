fd3a649d48214d05a9aac2819ff92f96cbe50908
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-14 17:01:59 +0300

    Update routes

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,14 +1,22 @@
-import ProductDetails from '@/components/products/ProductDetails'
 import axios from 'axios';
-import React from 'react';
+import ProductDetails from '@/components/products/ProductDetails';
+import mongoose from 'mongoose';
+import { redirect } from 'next/navigation';
 
 const getProductDetails = async (id) => {
     const { data } = await axios.get(`${process.env.API_URL}/api/products/${id}`);
+    
     return data?.product;
 };
 
 const ProductDetailsPage = async ({ params }) => {
-    const product = await getProductDetails(params.id);
+    const isValidId = mongoose.isValidObjectId(params?.id);
+
+    if (!isValidId) {
+      return redirect("/");
+    }
+
+    const product = await getProductDetails(params?.id);
 
     return (
         <ProductDetails product={product} />
