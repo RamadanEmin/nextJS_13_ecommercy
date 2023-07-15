50ba7e8ad6697cd4048b374811d0f60b08f3a2ce
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-15 14:41:00 +0300

    Added 'Delete product' function and api

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -86,3 +86,21 @@ export const updateProduct = async (req, res, next) => {
         product
     });
 };
+
+export const deleteProduct = async (req, res, next) => {
+    let product = await Product.findById(req.query.id);
+
+    if (!product) {
+        return next(new ErrorHandler("Product not found.", 404));
+    }
+
+    for (let i = 0; i < product.images.length; i++) {
+        const res = await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true
+    });
+};
--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -2,12 +2,13 @@ import nc from "next-connect";
 import dbConnect from "@/backend/config/dbConnect";
 import onError from "@/backend/middlewares/errors";
 import { authorizeRoles, isAuthenticatedUser } from "@/backend/middlewares/auth";
-import { updateProduct } from "@/backend/controllers/productControllers";
+import { deleteProduct, updateProduct } from "@/backend/controllers/productControllers";
 
 const handler = nc({ onError });
 
 dbConnect();
 
 handler.use(isAuthenticatedUser, authorizeRoles("admin")).put(updateProduct);
+handler.use(isAuthenticatedUser, authorizeRoles("admin")).delete(deleteProduct);
 
 export default handler;
