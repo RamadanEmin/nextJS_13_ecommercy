34d0c6daeec9fb8b8841a24cc1dc3e796c0ee8ac
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-17 16:34:16 +0300

    Update Products component

--- components/admin/Products.jsx
+++ components/admin/Products.jsx
@@ -1,9 +1,26 @@
+'use client';
+
+import { useContext, useEffect } from "react";
 import Link from "next/link";
+import { toast } from "react-toastify";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import CustomPagination from "../layouts/CustomPagination";
+import ProductContext from "@/context/ProductContext";
 
 const Products = ({ data }) => {
+    const { deleteProduct, error, clearErrors } = useContext(ProductContext);
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            clearErrors();
+        }
+    }, [error]);
+
+    const deleteHandler = (id) => {
+        deleteProduct(id);
+    };
 
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -48,6 +65,7 @@ const Products = ({ data }) => {
                                     </Link>
                                     <a
                                         className="px-2 py-2 inline-block text-red-600 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer"
+                                        onClick={() => deleteHandler(product?._id)}
                                     >
                                         <FontAwesomeIcon icon={faTrash} aria-hidden="true" />
                                     </a>
