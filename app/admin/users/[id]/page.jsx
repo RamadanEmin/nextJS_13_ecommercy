661e784cc5ea4c519345f650bf8a1301f8d413ba
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-27 01:41:03 +0300

    Added 'Admin user details' page

---
+++ app/admin/users/[id]/page.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import mongoose from "mongoose";
+import UpdateUser from "@/components/admin/UpdateUser";
+
+const getUser = async (id) => {
+    const nextCookies = cookies();
+    const nextAuthSessionToken = nextCookies.get('next-auth.session-token');
+
+    const { data } = await axios.get(`${process.env.API_URL}/api/admin/users/${id}`,
+        {
+            headers: {
+                Cookie: `next-auth.session-token=${nextAuthSessionToken?.value}`,
+            },
+        });
+
+    return data;
+};
+
+const AdminUserDetailsPage = async ({ params }) => {
+    const isValidId = mongoose.isValidObjectId(params?.id);
+
+    if (!isValidId) {
+        return redirect("/admin/users");
+    }
+
+    const data = await getUser(params?.id);
+
+    return <UpdateUser user={data?.user} />;
+};
+
+export default AdminUserDetailsPage;
