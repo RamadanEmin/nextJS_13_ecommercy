ece47ef7848d0f95e4cecb6aa0ac093d2d5a6956
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-07-30 00:23:39 +0300

    Update 'NewReview' component

--- components/review/NewReview.jsx
+++ components/review/NewReview.jsx
@@ -1,6 +1,40 @@
-import StarRatings from "react-star-ratings";
+'use client';
 
-const NewReview = () => {
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import StarRatings from "react-star-ratings";
+import AuthContext from "@/context/AuthContext";
+import ProductContext from "@/context/ProductContext";
+import { getUserReview } from "@/helpers/helpers";
+
+const NewReview = ({ product }) => {
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
+
+    const { user } = useContext(AuthContext);
+    const { error, clearErrors, postReview } = useContext(ProductContext);
+
+    useEffect(() => {
+        const userReview = getUserReview(product?.reviews, user?._id);
+
+        if (userReview) {
+            setRating(userReview?.rating);
+            setComment(userReview?.comment);
+        }
+
+        if (error) {
+            toast.error(error);
+            clearErrors();
+        }
+    }, [error, user])
+
+
+    const submitHandler = () => {
+        const reviewData = { rating, comment, productId: product?._id };
+        
+        postReview(reviewData);
+    };
+
     return (
         <div>
             <hr className="my-4" />
@@ -10,9 +44,11 @@ const NewReview = () => {
             <div className="mb-4 mt-3">
                 <div className="ratings">
                     <StarRatings
+                        rating={rating}
                         starRatedColor="#ffb829"
                         numberOfStars={5}
                         name="rating"
+                        changeRating={(e) => setRating(e)}
                     />
                 </div>
             </div>
@@ -23,12 +59,15 @@ const NewReview = () => {
                     className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-1/3"
                     placeholder="Your review"
                     name="description"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
                     required
                 ></textarea>
             </div>
 
             <button
                 className="mt-3 mb-5 px-4 py-2 text-center inline-block text-white bg-yellow-500 border border-transparent rounded-md hover:bg-yellow-600 w-1/3"
+                onClick={() => submitHandler()}
             >
                 Post Review
             </button>
