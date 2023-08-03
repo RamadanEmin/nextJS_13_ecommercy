8322d6d5cd498d561e67172f685faf1cb140481a
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-03 22:18:11 +0300

    Update Product Details component

--- components/products/ProductDetails.jsx
+++ components/products/ProductDetails.jsx
@@ -4,15 +4,23 @@ import { useRef, useContext, useEffect } from "react";
 import StarRatings from "react-star-ratings";
 import BreadCrumbs from "../layouts/BreadCrumbs";
 import CartContext from "@/context/CartContext";
+import OrderContext from "@/context/OrderContext";
 import NewReview from "../review/NewReview";
+import Reviews from "../review/Reviews";
 
 const ProductDetails = ({ product }) => {
     const { addItemToCart } = useContext(CartContext);
+    const { canUserReview, canReview } = useContext(OrderContext);
+
     const imgRef = useRef(null);
 
     const setImgPreview = (url) => {
         imgRef.current.src = url;
     };
+
+    useEffect(() => {
+        canUserReview(product?._id)
+    }, []);
 
     const inStock = product?.stock >= 1;
 
@@ -143,13 +151,14 @@ const ProductDetails = ({ product }) => {
                         </main>
                     </div>
 
-                   <NewReview product={product} />
+                    {canReview && <NewReview product={product} />}
                     <hr />
 
                     <div className="font-semibold">
                         <h1 className="text-gray-500 review-title mb-6 mt-10 text-2xl">
                             Other Customers Reviews
                         </h1>
+                        <Reviews reviews={product?.reviews} />
                     </div>
                 </div>
             </section>
